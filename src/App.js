13d8fa38a90c5c2cfa634c460775bd33c42f2835
syoungee <liahwang@tecace.com>
2024-01-02 15:50:57 +0900

    feature / add react router to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import VideoList from './components/VideoList';
 import VideoPage from './components/VideoPage';
+import NotFound from './components/NotFound';
+
+// / 👉 <Videos>🔥
+// /videos 👉 <Videos>🔥
+// /videos/query 👉 <Videos>🔍
+// /videos/watch/id 👉 <VideoDetail>
 
 function App() {
   return (
     <div className="App">
-      {/* <VideoList /> */}
-      <VideoPage />
+      <BrowserRouter>
+        <Routes>
+          {/* <VideoList /> */}
+          <Route path="/" element={<VideoList></VideoList>} />
+          <Route path="/videos/watch/id" element={<VideoPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
