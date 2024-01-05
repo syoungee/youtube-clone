868d85e58993514a6450ae76084e49e976ecf352
syoungee <liahwang@tecace.com>
2024-01-05 12:31:16 +0900

    chore / 안쓰는 코드 제거

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import SearchHeader from '../components/SearchHeader';
