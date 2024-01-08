061bb131659e372ce31976c4953bea5da5be9045
syoungee <liahwang@tecace.com>
2024-01-08 14:56:03 +0900

    feature / 상세 페이지 관련 영상들 컴포넌트

---
+++ src/components/RelatedVideos.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useYoutubeApi } from '../context/YoutubeApiContext';
+import { useQuery } from '@tanstack/react-query';
+import styles from '../components/VideoList.module.css';
+import { formatAgo } from '../util/date';
+
+export default function RelatedVideos({ id }) {
+  const { youtube } = useYoutubeApi();
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery({
+    queryKey: ['related', id],
+    queryFn: () => youtube.relatedVideos(id),
+  });
+
+  return (
+    <div>
+      Related Videos
+      {id}
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Something is wrong 😖</p>}
+      <ul className={styles['video-container']}>
+        {videos &&
+          videos.map((video) => (
+            <li key={video.id || video.id?.videoId}>
+              <img src={video.snippet.thumbnails.medium.url} alt={video.snippet.title} className={styles['thumbnail']} />
+              <div>
+                <p className="font-semibold my-2 line-clamp-2">{video.snippet.title}</p>
+                <p className="text-sm opacity-80">{video.snippet.channelTitle}</p>
+                <p className="text-sm opacity-80">{formatAgo(video.snippet.publishedAt, 'ko')}</p>
+              </div>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+}
