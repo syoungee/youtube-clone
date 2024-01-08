bcb96fdd0c0c6e2a1f42f492645ce1c5784450b6
syoungee <liahwang@tecace.com>
2024-01-08 14:55:45 +0900

    feature / 채널 데이터 컴포넌트

---
+++ src/components/ChannelInfo.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useYoutubeApi } from '../context/YoutubeApiContext';
+import { useQuery } from '@tanstack/react-query';
+
+export default function ChannelInfo({ id, name }) {
+  const { youtube } = useYoutubeApi();
+  const { error, isLoading, data: url } = useQuery({ queryKey: ['channel', id], queryFn: () => youtube.channelImageUrl(id) });
+
+  return (
+    <div>
+      {url && <img src={url} alt={name} />}
+      <p>{name}</p>
+    </div>
+  );
+}
